bc415960e06510ae462e4ac4a790ce5b072e8a22
ashcross <86404620+ashcross@users.noreply.github.com>
2025-02-11 21:16:43 +1300

    Update Header.tsx
    remove margin bottom

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="bg-white/60 shadow-sm py-6 backdrop-blur-sm absolute top-0 left-0 right-0 z-50">
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-center justify-center">
-          <Link to="/" className="mb-4">
+          <Link to="/" className="">
             <img 
               src="/logo.png" 
               alt="CrossWorks Lawnmowing & Property Care" 
